4de0075b1c405e79f3e851b0c044dac77606cfe3
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2025-02-26 11:47:43 +0800

    server

--- model-server.py
+++ model-server.py
@@ -1,43 +1,47 @@
 import os
+from swift.llm import InferEngine, InferRequest, PtEngine, RequestConfig
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from fastapi import FastAPI, UploadFile, Form, File
+
+app = FastAPI()
+
+from io import BytesIO
+from swift.plugin import InferStats
+from PIL import Image
 
 
 def infer_stream(engine: 'InferEngine', infer_request: 'InferRequest'):
     request_config = RequestConfig(max_tokens=128, temperature=0, stream=True)
     metric = InferStats()
     gen = engine.infer([infer_request], request_config, metrics=[metric])
-    query = infer_request.messages[0]['content']
-    print(f'query: {query}\nresponse: ', end='')
+
     for resp_list in gen:
         if resp_list[0] is None:
             continue
         print(resp_list[0].choices[0].delta.content, end='', flush=True)
-    print()
-    print(f'metric: {metric.compute()}')
 
 
 if __name__ == '__main__':
-    from swift.llm import InferEngine, InferRequest, PtEngine, RequestConfig
-    from swift.plugin import InferStats
-    from PIL import Image
-    model = rf'{os.getcwd()}/model/Qwen/Qwen2___5-VL-3B-Instruct'  # m\模型路径
-    engine = PtEngine(model, max_batch_size=64)
-    import os
-    image = Image.open(f"{os.getcwd()}/input-image/NSHM_PHOTO_2024_7_28_00_11_24.jpg")
+    prompt_text = "描述这张图片"
+    image = Image.open(f"{os.getcwd()}/input-image/NSHM_PHOTO_2024_8_19_23_33_19.jpg")
+    print(image.width,image.height)
     new_width = 800
     scale_ratio = new_width / image.width
     new_height = int(image.height * scale_ratio)
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "image": image.resize((new_width, new_height), Image.LANCZOS)
-                },
-                {"type": "text", "text": "动物是什么"},
-            ],
-        }
-    ]
-    infer_stream(engine, InferRequest(messages=messages))
+    print(image.resize((new_width, new_height), Image.LANCZOS))
+    # model = rf'{os.getcwd()}/model/Qwen/Qwen2___5-VL-3B-Instruct'  # m\模型路径
+    # engine = PtEngine(model, max_batch_size=64)
+    # messages = [
+    #     {
+    #         "role": "user",
+    #         "content": [
+    #             {
+    #                 "type": "image",
+    #                 "image": image.resize((new_width, new_height), Image.LANCZOS)
+    #             },
+    #             {"type": "text", "text": f"{prompt_text}"},
+    #         ],
+    #     }
+    # ]
+    # infer_stream(engine, InferRequest(messages=messages))
--- model-swift-server.py
+++ model-swift-server.py
@@ -1,47 +1,64 @@
-from swift.llm import InferRequest, InferClient, RequestConfig
+import os
+from io import BytesIO
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from swift.llm import InferRequest, PtEngine, RequestConfig, load_image
 from swift.plugin import InferStats
 
+model = rf'{os.getcwd()}/model/Qwen/Qwen2___5-VL-3B-Instruct'  # m\模型路径
+engine = PtEngine(model, max_batch_size=64)
+from fastapi import FastAPI, UploadFile, Form, File
+from fastapi.responses import StreamingResponse
 
-engine = InferClient(host='127.0.0.1', port=8000)
-print(f'models: {engine.models}')
-metric = InferStats()
-request_config = RequestConfig(max_tokens=512, temperature=0)
+app = FastAPI()
+from PIL import Image
+from pathlib import Path
 
-# 这里使用了3个infer_request来展示batch推理
-# 支持传入本地路径、base64和url
-infer_requests = [
-    InferRequest(messages=[{'role': 'user', 'content': 'who are you?'}]),
-    InferRequest(messages=[{'role': 'user', 'content': '<image><image>两张图的区别是什么？'}],
-                 images=['http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/cat.png',
-                         'http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/animal.png']),
-    InferRequest(messages=[{'role': 'user', 'content': '<video>describe the video'}],
-                 videos=['https://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/baby.mp4']),
-]
+input_folder = Path("input-image")
 
-resp_list = engine.infer(infer_requests, request_config, metrics=[metric])
-print(f'response0: {resp_list[0].choices[0].message.content}')
-print(f'response1: {resp_list[1].choices[0].message.content}')
-print(f'response2: {resp_list[2].choices[0].message.content}')
-print(metric.compute())
-metric.reset()
+c
+@app.post("/chat")
+async def chat(prompt_text: str = Form(), prompt_image: UploadFile = File()):
+    image_buffer = BytesIO()
+    # 读取文件内容到缓冲区
+    contents = await prompt_image.read()
+    image_buffer.write(contents)
+    image = load_image(image_buffer)
+    new_width = 800 if image.width >= 800 else 400
+    scale_ratio = new_width / image.width
+    new_height = int(image.height * scale_ratio)
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": image.resize((new_width, new_height), Image.LANCZOS)
+                },
+                {"type": "text", "text": f"{prompt_text}"},
+            ],
+        }
+    ]
+    return StreamingResponse(
+        infer_stream(InferRequest(messages=messages), image_buffer),
+        media_type="text/plain"
+    )
 
-# base64
-import base64
-import requests
-resp = requests.get('https://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/baby.mp4')
-base64_encoded = base64.b64encode(resp.content).decode('utf-8')
-messages = [{'role': 'user', 'content': [
-    {'type': 'video', 'video': f'data:video/mp4;base64,{base64_encoded}'},
-    {'type': 'text', 'text': 'describe the video'}
-]}]
-infer_request = InferRequest(messages=messages)
-request_config = RequestConfig(max_tokens=512, temperature=0, stream=True)
-gen = engine.infer([infer_request], request_config, metrics=[metric])
-print(f'response0: ', end='')
-for chunk_list in gen:
-    chunk = chunk_list[0]
-    if chunk is None:
-        continue
-    print(chunk.choices[0].delta.content, end='', flush=True)
-print()
-print(metric.compute())
+
+def infer_stream(infer_request: 'InferRequest', image_buffer):
+    request_config = RequestConfig(max_tokens=128, temperature=0, stream=True)
+    metric = InferStats()
+    try:
+        gen = engine.infer([infer_request], request_config, metrics=[metric])
+        for resp_list in gen:
+            if resp_list[0] is None:
+                continue
+            yield str(resp_list[0])
+    finally:
+        image_buffer.close()
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=10095)
